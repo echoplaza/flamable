fc2a7c77ca201af33c75f6d7cafb86cbb5b0aafc
echoplaza <trade.well.operator@gmail.com>
2025-11-19 13:17:37 -0600

    min build copied

---
+++ src/components/ItemGrid.jsx
@@ -0,0 +1,11 @@
+import ItemCard from "./ItemCard"
+
+const ItemGrid = () => {
+	return (
+        <div className='bg-primary-color rounded-3xl overflow-hidden border-8 border-primary-color shadow-3xl '>
+			<ItemCard />
+		</div>
+	)
+}
+
+export default ItemGrid

93e0174fb2075707428787678ba7d62dc4337e58
echoplaza <trade.well.operator@gmail.com>
2025-11-20 18:04:12 -0600

    esthetic updates
    Saw dust background
    
    See-through text
    
    Itemcard image also sawdust

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,4 +1,4 @@
-import Dwarf from '../assets/sam af1.jpg'
+import sawdust from '../assets/sawdust.png'
 
 const ItemCard = () => {
 	return (
@@ -7,14 +7,12 @@ const ItemCard = () => {
 			<div className='flex flex-col sm:flex-row justify-center items-center w-10/12'>
 				<img
 					className='md:w-1/2 sm:my-20 px-4'
-					src={Dwarf}
+					src={sawdust}
 					alt='Image of a dungeon door'
 					
 				/>
-				<h1 className=' md:w-5/6 font-heroking md:text-4xl text-2xl my-20 text-primary-white  '>
-					Greetings, valiant voyagers of the vast and varied realms! Welcome to
-					Bloodbath and Beyond, the ultimate emporium for every
-					adventurer&apos;s appetite.
+				<h1 className="md:w-5/6 font-heroking md:text-4xl text-2xl my-20 text-sawdust font-outline-1">
+					This is flamable. 
 				</h1>
 			</div>
 	
